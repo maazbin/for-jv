a58ad16888bfd1526e5731fa179781ce970c73ba
maazbin <maaz.mstqm@gmail.com>
2022-06-22 23:18:37 +0500

    first com

---
+++ conditions.py
@@ -0,0 +1,18 @@
+# We are going to create a calculator 
+
+# Taking input from the user
+
+num1 = float(input("Give me the first number : "))
+num2 = float(input("Give me the second number :"))
+
+# Addition
+
+
+
+
+# Substraction
+
+
+#division
+
+#multiplication
