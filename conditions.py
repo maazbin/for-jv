e6df06706172f2dd37a1fd3c917f3489fb9c5fcf
maazbin <maaz.mstqm@gmail.com>
2022-06-27 23:17:36 +0500

    PDF added

--- conditions.py
+++ conditions.py
@@ -6,7 +6,9 @@ num1 = float(input("Give me the first number : "))
 num2 = float(input("Give me the second number :"))
 
 # We take an operator
-
+if num2 == 0. :
+    print(num2)
+    # return 0
 operator = input("Give me an operator like +,-,*,/ :")
 
 # Addition
---
+++ if.py
@@ -0,0 +1,9 @@
+# x = 50 
+# y = 100
+
+if 50>100 : #if  condition here is True
+    print("greater") #this line will run
+elif 50 > 60:
+    print("No 60")
+elif 50 > 49:
+    print("Yes,49")
