f42c0579017157de64622218dc01ef72e416a916
maazbin <maaz.mstqm@gmail.com>
2022-06-28 23:51:01 +0500

    more array explaination ,indexes

---
+++ arrays.py
@@ -0,0 +1,16 @@
+# # Cities = ['']
+
+# city1 = 'LA'
+# city2 = 'New York'
+# city3 = 'Karachi'
+
+# # 100 cities
+
+x = [5,7,8,9,11,6,4]
+
+# x = 5
+
+# print(x[1+1]) #index starts from 0
+print(x[5-2])
+
+# x[index]
