af5d3b7cf212476add4440b3dccf92a8f1c6dbdc
maazbin <maaz.mstqm@gmail.com>
2022-07-04 23:24:16 +0500

    negative number count

--- negative.py
+++ negative.py
@@ -1,1 +1,8 @@
-numbers = [1,2,3,99,-7,-8,0,-44,-10]
+numbers = [1,2,3,99,-7,-8,0,-44,-10] #list of numbers
+
+for i in numbers:
+    print(i)
+
+    if number is less than 0 
+    
+    count  = count +1
