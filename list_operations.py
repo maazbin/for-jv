0426dd5d078d45b2031390b6deb67bcde0226511
maazbin <maaz.mstqm@gmail.com>
2022-07-06 23:17:36 +0500

    reverse the list

--- list_operations.py
+++ list_operations.py
@@ -1,8 +1,14 @@
-numbers = [1,2,3,99,-7,-8,0,-44,-10] #list of numbers
+numbers = [1,2,3,99,-7] #list of numbers
 
-print("The list has ",len(numbers)," values")
+# print("The list has ",len(numbers)," values")
 
 
-# print(numbers[len(numbers)-1])
+# print(numbers[len(numbers)])
+# print(numbers[len(numbers)])
 
-print(numbers[-3])
+# print(numbers[-3])
+
+for  i in range(len(numbers)):
+    print(numbers[len(numbers)-1], " i here is ", i)
+
+
