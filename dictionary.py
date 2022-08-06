0021f40ce1790539f56d59aefb1a0cc689f98b9b
maazbin <maaz.mstqm@gmail.com>
2022-08-06 23:14:05 +0500

    car features in a list

---
+++ dictionary.py
@@ -0,0 +1,40 @@
+
+
+mydict = {} # an empty dictionary
+
+mydict = {
+    "r":"read",
+    "b": "book",
+    "g": "granny",
+    "p" : "plane",
+    }
+
+# print(mydict['r'])
+# print(mydict['b'])
+# print(mydict['p'])
+
+
+
+
+
+
+# cities = {
+#     "JV":"Wellington",
+#     "Maaz": "Karachi",
+#     "Mark": "NYC",
+#     # "p" : "plane",
+#     }
+
+
+
+
+
+
+# print(cities['JV'])
+# print(cities['Mark'])
+# print(cities['Maaz'])
+
+# carColor = "red"
+# carModel = "2020"
+
+Features_of_car = ['red',445,'4','leather']
