83322e340604f9792fd341dbd558686b83ce6f02
maazbin <maaz.mstqm@gmail.com>
2022-08-11 00:00:47 +0500

    function examples

---
+++ func.py
@@ -0,0 +1,30 @@
+
+#hello function
+def hello():
+    print('hello,world')
+
+
+
+def sum_of_fives():
+    return 5+5
+
+def sum_of_numbers(number):
+    return number + number
+
+
+sums = sum_of_numbers(10)
+print(sums)
+
+    # print("hi",name)
+
+
+
+
+
+#call the function
+
+# hello()
+# sum_of_fives()
+
+# sums = sum_of_fives()
+# print(sum_of_fives())
