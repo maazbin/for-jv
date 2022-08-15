4ecef3e932f0809384932d43850630fe492beb0d
maazbin <maaz.mstqm@gmail.com>
2022-08-15 23:39:33 +0500

    loundry boy example

--- func.py
+++ func.py
@@ -8,10 +8,32 @@ def hello():
 def helloReeturn():
     return "I am a function which returns hello !"
 
-#calling function
-x = helloReeturn()
+
+# takes input
+def loundryBoy(dirty_cloth): # dirty cloths are -ve
+
+    # washed cloths
+    washed_cloths =   dirty_cloth
+    #when we wash them they become +ve
+    washed_cloths = washed_cloths * (-1)
+    # returns washed cloths
+    return washed_cloths
+
+
+
+#calling loundry boy
+# consider a -ve number as dirty cloths
+
+x = loundryBoy(-1)
 
 print(x)
+
+
+
+#calling function
+# x = helloReeturn()
+
+# print(x)
 
 # def sum_of_numbers(number):
 #     return number + number
