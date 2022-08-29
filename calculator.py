e67608c77174b1e50f295e3d23b80a90bb5227d2
maazbin <maaz.mstqm@gmail.com>
2022-08-29 23:10:39 +0500

    more function example + count numbers

---
+++ calculator.py
@@ -0,0 +1,23 @@
+def add():
+    pass
+
+def minus():
+    pass
+
+def mul():
+    pass
+
+def div():
+    pass
+
+
+
+num1 = float(input("give me num1"))
+num2 = float(input("give me num2"))
+
+operator = input("Give me an operation from *,/,+,-")
+
+if operator == '+':
+    print(add(naum1,num2))
+if operator == '-':
+    pass    
---
+++ func2.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+def count_num(num):
+    count = 0
+    for i in num:
+        count = count +1
+        # print()
+    return count
+
+
+num_list = [5,8,9,7,6]
+count_of_num = count_num(num_list)
+print(count_of_num)
