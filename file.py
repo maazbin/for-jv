9cfff06550d2e1e7b79a4b911637681b94f4357c
maazbin <maaz.mstqm@gmail.com>
2022-09-26 23:13:44 +0500

    filing

--- file.py
+++ file.py
@@ -1,4 +1,16 @@
-f = open("file.txt")
+
+''' 
+Modes to open a file: 
+
+1) Read the file 
+2) Write (into) the file
+3) execute the file (run the file)
+
+'''
+
+f = open("files_bank/hello.txt")
 
 # reading the data of file
 print(f.read())
+
+
