7a6f7068c4cc55d7c9bf8e770636c88d25849f90
maazbin <maaz.mustaqeem@highplains.io>
2023-03-15 22:33:55 +0500

    basic dictionary excersie

--- dictionary.py
+++ dictionary.py
@@ -1,56 +1,13 @@
+players = ['Maaz','JV','Taylor','Bilal','Adam']
+score = [101,55,88,55,66,]
+
+print(f'{players[0]} scored {score[0]}, {players[1]} scored {score[1]}')
 
 
-# mydict = {} # an empty dictionary
+new_scores = {
+    'Maaz':101,
+    'JV':55,
+    'Taylor': 88
+}
 
-# mydict = {
-#     "r":"read",
-#     "b": "book",
-#     "g": "granny",
-#     "p" : "plane",
-#     }
-
-
-
-
-# country = {
-#     "Russia" : "1000",
-#     "Nepal" : "1",
-#     "Canada" : "6",
-# }
-
-# # print(country["Russia"])
-
-# for x in country:
-#     print(country[x])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print('JV scored : ',new_scores['JV'])
